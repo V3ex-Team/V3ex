0c33df29300248b0299166d827b635715bf7e0b9
anthhub <anthhub@163.com>
2024-07-07 14:01:33 +0800

    feat: url

--- ui/src/utils/request.ts
+++ ui/src/utils/request.ts
@@ -31,13 +31,15 @@ import { floppyNavigation } from './floppyNavigation';
 import { isIgnoredPath, IGNORE_PATH_LIST } from './guard';
 
 const baseConfig = {
-  baseURL:
-    process.env.NODE_ENV === 'development'
-      ? process.env.REACT_APP_API_URL
-      : process.env.REACT_APP_API_URL,
+  baseURL: 'http://49.232.15.240:9081/',
+  // process.env.NODE_ENV === 'development'
+  //   ? process.env.REACT_APP_API_URL
+  //   : process.env.REACT_APP_API_URL,
   timeout: 10000,
   // withCredentials: true,
 };
+
+console.log('process.env.REACT_APP_API_URL', process.env.REACT_APP_API_URL);
 
 interface ApiConfig extends AxiosRequestConfig {
   // Configure whether to allow takeover of 404 errors
