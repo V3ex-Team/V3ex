5fb4887f7329edd1f3a7c9172687bba92d131e43
wastill <2225144254@qq.com>
2024-07-15 18:21:16 +0800

    1

--- internal/service/v3ex/v3ex_sign_service.go
+++ internal/service/v3ex/v3ex_sign_service.go
@@ -1,5 +1,16 @@
 package v3ex
 
+import (
+	"fmt"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
+	"math/big"
+)
+
+const (
+	_PrivateKey = "0x92db14e403b83dfe3df233f83dfa3a0d7096f21ca9b0d6d6b8d88b2b4ec1564e"
+)
+
 type V3exRepo interface {
 }
 
@@ -13,4 +24,40 @@ func NewV3exService(
 	return &V3exService{
 		v3exRepo: v3exRepo,
 	}
+}
+
+func Sign() {
+	// 获取链 ID
+	chainId := uint64(1) // 假设在主网上操作
+
+	// 生成交易 ID
+	to := common.HexToAddress("0x1234567890123456789012345678901234567890")
+	date := big.NewInt(1618304400) // 2021年4月13日
+	transId := crypto.Keccak256Hash(
+		new(big.Int).SetUint64(chainId).Bytes(),
+		to.Bytes(),
+		date.Bytes(),
+	).Hex()
+
+	// 生成哈希值
+	newAmount := big.NewInt(100)
+	hash := crypto.Keccak256Hash(
+		new(big.Int).SetUint64(chainId).Bytes(),
+		[]byte("CHECKIN"),
+		common.HexToHash(transId).Bytes(),
+		newAmount.Bytes(),
+		to.Bytes(),
+	).Hex()
+
+	privateKey, err := crypto.HexToECDSA(_PrivateKey)
+
+	signatureBytes, err := crypto.Sign(common.HexToHash(hash).Bytes(), privateKey)
+	if err != nil {
+		fmt.Println("Error signing hash:", err)
+		return
+	}
+
+	// 将签名结果拼接成完整的签名字符串
+	signature := "0x" + common.Bytes2Hex(signatureBytes[:64]) + common.Bytes2Hex([]byte{signatureBytes[64]})
+	fmt.Println("Signature:", signature)
 }
---
+++ internal/service/v3ex/v3ex_sign_service_test.go
@@ -0,0 +1,7 @@
+package v3ex
+
+import "testing"
+
+func TestSign(t *testing.T) {
+
+}
